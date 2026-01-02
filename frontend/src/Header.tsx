dfae78aad5e07ba38de8c4441160915043310311
Antony <a2020gk@outlook.com>
2026-01-02 21:40:08 +0300

    Ooo, fancy logo

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     const { setOpen } = useContext(SettingsContext)
     return <header>
         <ul>
-            <li><span className="box"><a href="/"><b>Тестовый стенд</b></a></span></li>
+            <li><span className="box"><a href="/"><img src="/favicon.ico" alt="favicon" width={17}/> <b>Тестовый стенд</b></a></span></li>
             <li><span className="box"><a href="#" onClick={() => setOpen(true)}>Настройки</a></span></li>
         </ul>
     </header>
